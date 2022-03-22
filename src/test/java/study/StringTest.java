d3f5cbd31d1be06b9ad273a420a5f3100d0c8ebc
조하선 <johaseon@johaseon-ui-MacBookPro.local>
2022-03-23 08:15:38 +0900

    String 클래스에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,42 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1");
+
+        String[] actual2 = "1".split(",");
+        assertThat(actual2).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("chatAt()로 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 예외가 발생한다.")
+    void charAt() {
+        assertThatThrownBy(() -> {
+            char actual = "abc".charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class);
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    char actual = "abc".charAt(3);
+                });
     }
 }
